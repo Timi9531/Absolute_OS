48c1609b4295814afeb4a15735c0fda835ee7934
Timi9531 <timur7746y@gmail.com>
2025-12-04 19:11:32 +0300

    added waybar power menu, added hypridle

--- ags/AS_widget.tsx
+++ ags/AS_widget.tsx
@@ -1,79 +1,75 @@
 import app from "ags/gtk4/app";
-import css from "./style.css"
-import { Accessor, createBinding, createState, For, With } from "gnim"
-import Gtk from "gi://Gtk?version=4.0"
-import Gdk from "gi://Gdk?version=4.0"
-import Pango from "gi://Pango?version=1.0"
-import Astal from "gi://Astal?version=4.0"
-import { FCProps } from "gnim"
-import GLib from "gi://GLib?version=2.0";
-import ModemManager from "gi://ModemManager?version=1.0";
+import css from "./style.css";
+import { Accessor, createBinding, createState, For, With } from "gnim";
+import Gtk from "gi://Gtk?version=4.0";
+import Gdk from "gi://Gdk?version=4.0";
+import Astal from "gi://Astal?version=4.0";
+import { FCProps } from "gnim";
+import SolverMenu from "./solver_menu";
 
 function delay(time: number) {
-  return new Promise(resolve => setTimeout(resolve, time));
+  return new Promise((resolve) => setTimeout(resolve, time));
 }
 /* sex */
 
 type HoverImageProps = FCProps<
   Gtk.Image,
   {
-    name?: string
-    file: string
-    normalSize: number
-    hoverSize: number
-
+    name?: string;
+    file: string;
+    normalSize: number;
+    hoverSize: number;
+    children?: JSX.Element;
+    onClick?: () => void;
   }
->
+>;
 
-function HoverImage({ name, file, normalSize, hoverSize }: HoverImageProps)
-{
-  const [currentSize, setCurrentSize] = createState(normalSize)
+function HoverImage({ name, file, normalSize, hoverSize, onClick }: HoverImageProps) {
+  const [currentSize, setCurrentSize] = createState(normalSize);
 
-  return(
-
-      <image 
+  return (
+    <image
       visible
-      name={name} 
+      name={name}
       file={file}
       pixelSize={currentSize}
       marginBottom={0}
-      marginStart={0}>
-
-        <Gtk.EventControllerMotion
-        onEnter={ async () => {
+      marginStart={0}
+    >
+      <Gtk.EventControllerMotion
+        onEnter={async () => {
           let diff = 0;
-          while(diff <= normalSize)
-            {
+          while (diff <= normalSize) {
             let normalSize_diff = normalSize + diff;
             await delay(1);
             setCurrentSize(normalSize_diff);
             diff++;
-            
-            }
           }
-        }
-        onLeave={ async () => {
+        }}
+        onLeave={async () => {
           let diff = 0;
-          while(diff <= normalSize) 
-          {
-            setCurrentSize(hoverSize-diff);
+          while (diff <= normalSize) {
+            setCurrentSize(hoverSize - diff);
             await delay(1);
             diff++;
           }
-        }} />
+        }}
+      />
 
-      </image> 
-
-  )
+      <Gtk.GestureClick
+        onPressed={() => {
+          onClick?.();
+        }}
+      />
+    </image>
+  );
 }
-
-
-
 
 function Solver(monitor = 0) {
 
+
   return (
-    <window 
+    <window
       visible
       name="main"
       application={app}
@@ -85,21 +81,25 @@ function Solver(monitor = 0) {
       anchor={Astal.WindowAnchor.RIGHT | Astal.WindowAnchor.TOP}
       margin_top={-39}
       margin_right={-4}
-      >
-          <HoverImage
-            name="solver"
-            file="./AbsoluteSolver1.png"
-            normalSize={64}
-            hoverSize={128} 
-            />
+    >
+      
+      
+      <HoverImage
+        name="solver"
+        file="./AbsoluteSolver1.png"
+        normalSize={64}
+        hoverSize={128}
+        onClick={}
+      ></HoverImage>
     </window>
-  )
+  );
 }
 
 app.start({
   css: css,
   main() {
-    Solver(0)
-    
+    Solver(0);
+    SolverMenu(0);
   },
-})
+});
+
---
+++ ags/solver_menu.tsx
@@ -0,0 +1,21 @@
+import app from "ags/gtk4/app";
+import css from "./style.css";
+import Gtk from "gi://Gtk?version=4.0";
+import Astal from "gi://Astal?version=4.0";
+import { FCProps, With } from "gnim";
+import { Accessor, createBinding, createState } from "gnim";
+
+export default function SolverMenu(monitor=0) {
+
+  const [isVisible, setVisible] = createState(false);
+
+  return (
+    <box>
+      
+    </box>
+      
+  );
+}
+
+
+
